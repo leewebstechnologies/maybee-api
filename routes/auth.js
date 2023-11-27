f0b765829db3bfafd245dbb6e908666b3ca5739b
leewebstechnologies <aliuajibade317@gmail.com>
2023-11-27 21:28:25 +0100

    jsonwebtoken added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken")
 
 // REGISTER
 router.post("/register", async (req, res) => {
