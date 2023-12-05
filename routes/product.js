ae95d75338728b50b357e25c0926e0327ea0c7ae
leewebstechnologies <aliuajibade317@gmail.com>
2023-12-05 05:39:15 +0100

    user route

---
+++ routes/product.js
@@ -0,0 +1,23 @@
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
